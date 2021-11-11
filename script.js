c63ed75cb5d6582c34b8f1655003fa193fe5df59
Sazanimes <ebinsanthosh@outlook.com>
2021-11-11 10:12:23 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,144 @@
+    //Function With Numbers
+    //Number 1
+    var timesClicked1 = 0;
+    function tt1(){
+        timesClicked1 += 1;
+    }
+    function counter1(){
+    var audio = document.getElementById('audio1');
+    if (timesClicked1 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 2
+    var timesClicked2 = 0;
+    function tt2(){
+        timesClicked2 += 1;
+    }
+    function counter2(){
+    var audio = document.getElementById('audio2');
+    if (timesClicked2 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 3
+    var timesClicked3 = 0;
+    function tt3(){
+        timesClicked3 += 1;
+    }
+    function counter3(){
+    var audio = document.getElementById('audio3');
+    if (timesClicked3 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 4
+    var timesClicked4 = 0;
+    function tt4(){
+        timesClicked4 += 1;
+    }
+    function counter4(){
+    var audio = document.getElementById('audio4');
+    if (timesClicked4 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 5
+    var timesClicked5 = 0;
+    function tt5(){
+        timesClicked5 += 1;
+    }
+    function counter5(){
+    var audio = document.getElementById('audio5');
+    if (timesClicked5 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 6
+    var timesClicked6 = 0;
+    function tt6(){
+        timesClicked6 += 1;
+    }
+    function counter6(){
+    var audio = document.getElementById('audio6');
+    if (timesClicked6 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 7
+    var timesClicked7 = 0;
+    function tt7(){
+        timesClicked7 += 1;
+    }
+    function counter7(){
+    var audio = document.getElementById('audio7');
+    if (timesClicked7 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 8
+    var timesClicked8 = 0;
+    function tt8(){
+        timesClicked8 += 1;
+    }
+    function counter8(){
+    var audio = document.getElementById('audio8');
+    if (timesClicked8 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 9
+    var timesClicked9 = 0;
+    function tt9(){
+        timesClicked9 += 1;
+    }
+    function counter9(){
+    var audio = document.getElementById('audio9');
+    if (timesClicked9 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    //Number 10
+    var timesClicked10 = 0;
+    function tt10(){
+        timesClicked10 += 1;
+    }
+    function counter10(){
+    var audio = document.getElementById('audio10');
+    if (timesClicked10 % 2 == 0){
+        audio.pause();
+        }
+    else{
+        audio.play();
+        }
+    }
+    setTimeout(function() {document.getElementById('preloader').style.animation="fadeOut 3s"}, 1000);
+    setTimeout(function() {document.getElementById('preloader').style.visibility="hidden";}, 3100);
+
